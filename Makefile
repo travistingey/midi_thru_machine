c6aa6a9601b7c3de89af0552fd316e9fdebb8127
travistingey <55222682+travistingey@users.noreply.github.com>
2025-07-24 22:21:23 -0400

    Add development scaffold

---
+++ Makefile
@@ -0,0 +1,21 @@
+LUA=lua
+LUAROCKS=luarocks
+
+install:
+brew install lua@5.3 || true
+brew install luarocks || true
+$(LUAROCKS) install busted || true
+$(LUAROCKS) install luacheck || true
+
+lint:
+luacheck Foobar.lua lib spec
+
+test:
+LUA_PATH='./?.lua;./lib/?.lua;.test/stubs/?.lua;;' busted
+
+ci: lint test
+
+deploy:
+rsync -av --exclude='.git' ./ $(PI_HOST):~/dust/code/Foobar
+
+.PHONY: install lint test ci deploy
---
+++ spec/example_spec.lua
@@ -0,0 +1,15 @@
+package.path = './?.lua;./lib/?.lua;.test/stubs/?.lua;' .. package.path
+require('norns')
+
+describe('utilities', function()
+  local u = require('lib/utilities')
+
+  it('removes duplicate entries', function()
+    assert.are.same({1,2,3}, u.removeDuplicates({1,2,2,3}))
+  end)
+
+  it('chains functions left-to-right', function()
+    local f = u.chain_functions({function(x) return x + 1 end, function(x) return x * 2 end})
+    assert.are.equal(4, f(1))
+  end)
+end)
